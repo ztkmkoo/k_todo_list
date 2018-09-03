6d55fd50b459abd7949babf66148255567d17f13
ztkmkoo <ztkmk@naver.com>
2018-09-04 02:38:03 +0900

    front-end v  1.3 - add log to handler - add table css and table example

--- front-end/src/main/java/com/ztkmkoo/todolist/frontend/handler/TestHandler.java
+++ front-end/src/main/java/com/ztkmkoo/todolist/frontend/handler/TestHandler.java
@@ -1,13 +1,16 @@
 package com.ztkmkoo.todolist.frontend.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 public class TestHandler {
 
     public Mono<ServerResponse> test(ServerRequest request) {
+        log.info("test: {}", request);
         return ServerResponse.ok().contentType(MediaType.TEXT_HTML). render("index");
     }
 }
