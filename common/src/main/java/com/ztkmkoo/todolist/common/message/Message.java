c02ce071b75e2de09292cfab3725db8cfbcc9332
ztkmkoo <ztkmk@naver.com>
2018-09-07 06:43:37 +0900

    common v 2.0 - new message. - new ignite type

--- common/src/main/java/com/ztkmkoo/todolist/common/message/Message.java
+++ common/src/main/java/com/ztkmkoo/todolist/common/message/Message.java
@@ -1,10 +1,10 @@
 package com.ztkmkoo.todolist.common.message;
 
+import com.ztkmkoo.todolist.common.model.ToDo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.util.List;
 
 public class Message {
@@ -19,11 +19,52 @@ public class Message {
     @Getter
     @AllArgsConstructor
     public static class InsertToDoAns implements Serializable {
+        private ToDo toDo;
+        private int totalCount;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class SelectToDoReq implements Serializable {
+        private int page;
+        private int countPerPage;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class SelectToDoAns implements Serializable {
+        private int page;
+        private int countPerPage;
+        private List<ToDo> toDoList;
+        private int totalPage;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class FinishToDoReq implements Serializable {
+        private int id;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class FinishToDoAns implements Serializable {
+        private int id;
+        private int isDone;
+        private String errorLog;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class UpdateToDoReq implements Serializable {
         private int id;
         private String toDo;
-        private List<Integer> refList;
-        private LocalDateTime registerDate;
-        private LocalDateTime modifyDate;
-        private int isDone;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class UpdateToDoAns implements Serializable {
+        private int id;
+        private boolean isDone;
+        private String errorLog;
     }
 }
--- common/src/main/java/com/ztkmkoo/todolist/common/model/ToDo.java
+++ common/src/main/java/com/ztkmkoo/todolist/common/model/ToDo.java
@@ -1,20 +1,21 @@
-package com.ztkmkoo.todolist.frontend.model;
+package com.ztkmkoo.todolist.common.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
+import java.io.Serializable;
+import java.sql.Timestamp;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ToDo {
+public class ToDo implements Serializable{
     private int id;
     private String toDo;
     private List<Integer> refList;
-    private LocalDateTime registerDate;
-    private LocalDateTime modifyDate;
+    private Timestamp registerDate;
+    private Timestamp modifyDate;
     private int isDone;
 }
