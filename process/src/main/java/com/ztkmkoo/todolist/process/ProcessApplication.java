1c65228ecabb519f7f6544608ca4acf9a0886914
ztkmkoo <ztkmk@naver.com>
2018-09-06 21:05:37 +0900

    process v 1.3 - modify handling InsertToDoReq logic - add logger to akka test actor

--- process/src/main/java/com/ztkmkoo/todolist/process/ProcessApplication.java
+++ process/src/main/java/com/ztkmkoo/todolist/process/ProcessApplication.java
@@ -4,12 +4,14 @@ import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 import com.ztkmkoo.todolist.common.message.Message;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-public class Application {
+public class ProcessApplication {
     public static void main(String[] args) throws InterruptedException {
         final ActorSystem actorSystem = ActorSystem.create("ActorSystem");
 
@@ -22,13 +24,17 @@ public class Application {
     }
 
     static class TestActor extends AbstractActor {
+        LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
 
         @Override
         public Receive createReceive() {
             return receiveBuilder()
                     .match(Message.InsertToDoReq.class, msg -> {
+                        log.info("Receive Message.InsertToDoReq {}", msg);
+
                         LocalDateTime now = LocalDateTime.now();
                         Message.InsertToDoAns ans = new Message.InsertToDoAns(3, msg.getToDo(), msg.getRefList(), now, now, 0);
+                        sender().tell(ans, self());
                     })
                     .matchAny(o -> {
                         System.out.println(o.toString());
